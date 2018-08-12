7c77af96583a28ebee47c1be479111dadeea8c60
BAMN <bernardo.melo@correo.policia.gov.co>
2018-08-11 22:09:27 -0500

    nuevamente se sube unas modificaciones incluyendo una classe nativa de cordova para la intro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { IonicStorageModule } from '@ionic/storage';
 import { MyApp } from './app.component';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Clipboard } from '@ionic-native/clipboard';
+
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
@@ -37,7 +39,7 @@ import { TabsPage } from '../pages/tabs/tabs';
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    
+    Clipboard
   ]
 })
 export class AppModule {}
