c6bedeeefb9de1c1c12438558f0af6b6b1374595
BAMN <bernardo.melo@correo.policia.gov.co>
2018-08-12 21:39:41 -0500

    Se agrega el arreglos a la pagina intro, se crea la pagina de checkin para el registro faltando hacerla funcional y mejoras en otras paginas

---
+++ src/pages/start/start.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the StartPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-start',
+  templateUrl: 'start.html',
+})
+export class StartPage {
+  item;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.item = navParams.data.item;
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad StartPage');
+  }
+
+}
